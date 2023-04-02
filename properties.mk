b0a6e08b4180a8fa61547db56469dd3a22e9c4b8
Martin Liu <liumartin@google.com>
2023-04-02 10:26:40 -0300

    jeter: Enable zram writeback
    * For Pixel devices, we have more working set and could
    bring launch time impact if we write back too fast. Thus,
    adjust the first time write back time to 24 hours which is
    aligned with periodic write back time.
    
    Signed-off-by: Martin Liu <liumartin@google.com>

--- properties.mk
+++ properties.mk
@@ -261,3 +261,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Wi-Fi
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0
+
+# ZRAM writeback
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.zram.mark_idle_delay_mins=60 \
+    ro.zram.first_wb_delay_mins=1440 \
+    ro.zram.periodic_wb_delay_hours=24
