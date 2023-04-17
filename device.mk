652f6388e10ea2210230626a5a5eaa293248f8a7
Vhmit <praxedes.9630@gmail.com>
2023-04-17 11:59:42 -0300

    jeter: Update script dependencies
    * Putting the camera dependencies in the script
    * Build Moto Actions
    
    Signed-off-by: Vhmit <praxedes.9630@gmail.com>

--- device.mk
+++ device.mk
@@ -224,10 +224,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
 # MotoActions
-ifeq ($(filter %ahannah %rhannah,$(TARGET_PRODUCT)),)
 PRODUCT_PACKAGES += \
     MotoActions
-endif
 
 # OMX
 PRODUCT_PACKAGES += \
