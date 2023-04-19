8ffdf72450d0bc5ef9cc7a9ce0fd373f0bf855e7
karthick111 <karthickrko61@gmail.com>
2023-04-19 00:14:50 -0300

    jeter: Update media stack to R
    Change-Id: I0ea5f1cb32b93971a7772b62e62190479fd94fd7
    Signed-off-by: karthick111 <karthickrko61@gmail.com>

--- device.mk
+++ device.mk
@@ -236,10 +236,13 @@ PRODUCT_PACKAGES += \
     libOmxAmrEnc \
     libOmxCore \
     libOmxEvrcEnc \
+    libOmxG711Enc \
     libOmxQcelp13Enc \
+    libOmxSwVencHevc \
     libOmxVdec \
     libOmxVenc \
-    libstagefrighthw
+    libstagefrighthw \
+    libstagefright_soft_flacdec
 
 # Permissions
 PRODUCT_COPY_FILES += \
