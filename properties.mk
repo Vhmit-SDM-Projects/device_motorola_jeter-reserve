davidgithub315/device_motorola_jeter

jeter: Set manufacturer and soc properties
Set soc model and manufacturer properties

Change-Id: I9ecc1cb75a415f5df147d47234fd9d8b69e1e6b5

properties.mk
@@ -102,6 +102,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapminfree=4m \
     dalvik.vm.heapmaxfree=8m
 
+# Soc
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.soc.manufacturer=QTI \
+    ro.soc.model=MSM8937
+
 # Debug
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.dbg.volte_avail_ovr=1 \

Vhmit-SDM-Projects/device_motorola_jeter-reserve

properties.mk
@@ -106,6 +106,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapminfree=4m \
     dalvik.vm.heapmaxfree=8m
 
+# Soc
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.soc.manufacturer=Qualcomm \
+    ro.soc.model=MSM8937
+
 # Debug
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.dbg.volte_avail_ovr=1 \