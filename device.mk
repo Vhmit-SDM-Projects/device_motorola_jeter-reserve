b26cd2c014a39206cd23e32aca30a95716b118da
vivekachooz <vivekachooz@gmail.com>
2023-05-16 01:13:44 -0300

    jeter: Provide power stats on vendor

--- device.mk
+++ device.mk
@@ -271,6 +271,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service \
     android.hardware.power.stats@1.0-service.mock \
+    android.hardware.power.stats@1.0.vendor \
     android.hardware.power@1.2.vendor
 
 # Perf
