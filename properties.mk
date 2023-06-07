af149e23ba6c46c6c1a6ed40acc55a38b8eda142
Vhmit <praxedes.9630@gmail.com>
2023-06-07 19:52:00 -0300

    jeter: for better dalvik heap size

--- properties.mk
+++ properties.mk
@@ -96,13 +96,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.dex2oat64.enabled=true \
     dalvik.vm.heapstartsize=8m \
     dalvik.vm.heapgrowthlimit=192m \
     dalvik.vm.heapsize=512m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=512k \
-    dalvik.vm.heapmaxfree=8m
+    dalvik.vm.heaptargetutilization=0.6 \
+    dalvik.vm.heapminfree=8m \
+    dalvik.vm.heapmaxfree=16m
 
 # Fastbootd
 PRODUCT_PROPERTY_OVERRIDES += \
